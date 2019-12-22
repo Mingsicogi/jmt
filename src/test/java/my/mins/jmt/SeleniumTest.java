3932221534bef98a172049d4db61bbac3c6f3fc6
minssogi <codedoctor119@gmail.com>
2019-12-22 21:43:12 +0900

    + Add Initial Commit

--- src/test/java/my/mins/jmt/SeleniumTest.java
+++ src/test/java/my/mins/jmt/SeleniumTest.java
@@ -22,27 +22,29 @@ public class SeleniumTest {
 
 	@Before
 	public void before() {
-		System.setProperty("webdriver.gecko.driver", "drivers/geckodriver");
+		System.setProperty("webdriver.gecko.driver", "drivers/geckodriver.exe");
+//		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
 	}
 	@Test
 	public void selenium_example() {
-		WebDriver driver = new ChromeDriver(); // 브라우저 실행
-		driver.get("https://accounts.google.com/signin/v2/identifier"); // 로그인 페이지로 이동 합니다.
-		WebElement id = driver.findElement(By.id("id"));
+//		WebDriver driver = new ChromeDriver(); // 브라우저 실행
+		WebDriver driver = new FirefoxDriver();
+		driver.get("http://localhost:8080/v1/login"); // 로그인 페이지로 이동 합니다.
+		WebElement id = driver.findElement(By.name("username"));
 		id.clear();
-		id.sendKeys("msjeon529"); // 아이디 입력 필드에 '아이디'를 입력 합니다.
-		WebElement pw = driver.findElement(By.id("pw"));
+		id.sendKeys("minssogi"); // 아이디 입력 필드에 '아이디'를 입력 합니다.
+		WebElement pw = driver.findElement(By.name("password"));
 		pw.clear();
 		pw.sendKeys("vlrmal1206!"); // 비빌번호 입력 필드에 '비밀번호'를 입력 합니다.
-		WebElement button = driver.findElement(By.cssSelector("#login > form > fieldset > button"));
+		WebElement button = driver.findElement(By.cssSelector(".btn.btn-default.submit"));
 		button.submit(); // Form 전송.
 		// Form 전송 후 로그인 완료 페이지로 이동하여 페이지가 완전히 랜더링 될 때 까지 대기 헙니다.
-		new WebDriverWait(driver, 10).until(ExpectedConditions.urlToBe("로그인 완료 URL"));
-		new WebDriverWait(driver, 10).until((ExpectedCondition<Boolean>)d -> {
-			assert d != null;
-			return ((JavascriptExecutor) d).executeScript("return jQuery.active == 0").equals(Boolean.TRUE);
-		});
-		driver.get("Scraping 대상 URL"); // Scraping 할 페이지로 이동 합니다.
+//		new WebDriverWait(driver, 10).until(ExpectedConditions.urlToBe("로그인 완료 URL"));
+//		new WebDriverWait(driver, 10).until((ExpectedCondition<Boolean>)d -> {
+//			assert d != null;
+//			return ((JavascriptExecutor) d).executeScript("return jQuery.active == 0").equals(Boolean.TRUE);
+//		});
+		driver.get("https://gdtbgl93.tistory.com/"); // Scraping 할 페이지로 이동 합니다.
 		System.out.println(driver.getPageSource()); // 페이지 소스를 Scraping 합니다.
 		driver.quit(); // 브라우저 종료
 	}
