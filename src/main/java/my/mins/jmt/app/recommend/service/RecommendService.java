8edb3d8ef201b176b2f751c9881519d8a75c7ba6
minssogi <codedoctor119@gmail.com>
2019-12-23 14:26:18 +0900

    + Service interface define

--- src/main/java/my/mins/jmt/app/recommend/service/RecommendService.java
+++ src/main/java/my/mins/jmt/app/recommend/service/RecommendService.java
@@ -1,4 +1,4 @@
 package my.mins.jmt.app.recommend.service;
 
-public interface FoodRecommend {
+public interface RecommendService {
 }
---
+++ src/main/java/my/mins/jmt/app/recommend/service/impl/RecommendServiceImpl.java
@@ -0,0 +1,8 @@
+package my.mins.jmt.app.recommend.service.impl;
+
+import my.mins.jmt.app.recommend.service.RecommendService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RecommendServiceImpl implements RecommendService {
+}
