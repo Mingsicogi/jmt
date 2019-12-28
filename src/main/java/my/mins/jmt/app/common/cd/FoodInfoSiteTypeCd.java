8607fb1339d1b0b1a347cfdce1a001fd97b98352
minssogi <codedoctor119@gmail.com>
2019-12-28 23:18:13 +0900

    + code refactor

--- src/main/java/my/mins/jmt/app/common/cd/FoodInfoSiteTypeCd.java
+++ src/main/java/my/mins/jmt/app/common/cd/FoodInfoSiteTypeCd.java
@@ -2,6 +2,7 @@ package my.mins.jmt.app.common.cd;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import my.mins.jmt.app.common.constants.CommonConstant;
 import my.mins.jmt.app.common.constants.FoodConstant;
 
@@ -16,21 +17,17 @@ import java.util.function.BiFunction;
 @AllArgsConstructor
 public enum FoodInfoSiteTypeCd {
 	DINING_CODE("https://www.diningcode.com",
-		"https://www.diningcode.com/isearch.php?query=%s",
-		(location, foodType) -> String.format(FoodInfoSiteTypeCd.DINING_CODE.getSearchUrlForm(), location + CommonConstant.SPACE + foodType.name() + FoodConstant.GOOD_FOOD_STORE)
+		(location, foodType) -> String.format("https://www.diningcode.com/isearch.php?query=%s", location + CommonConstant.SPACE + foodType + FoodConstant.GOOD_FOOD_STORE)
 	),
 
 	MANGO_PLATE("https://www.mangoplate.com",
-		"https://www.mangoplate.com/search/%s",
-		(location, foodType) -> String.format(FoodInfoSiteTypeCd.DINING_CODE.getSearchUrlForm(), location + CommonConstant.DASH + foodType.name())
+		(location, foodType) -> String.format("https://www.mangoplate.com/search/%s", location + CommonConstant.DASH + foodType.name())
 	),
 
 	TRIP_ADVISOR("",
-		"",
 		(location, foodType) -> location + foodType.getKrNm()
 	);
 
 	private String mainUrl;
-	private String searchUrlForm;
 	private BiFunction<String, FoodTypeCd, String> getSearchUrlFunction;
 }
