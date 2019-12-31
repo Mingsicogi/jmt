d7cdffcea1a70c09e75b35057c188843f8e9be33
minssogi <codedoctor119@gmail.com>
2019-12-31 12:54:17 +0900

    + Message send function implement

--- src/main/java/my/mins/jmt/app/send/controller/WebHook.java
+++ src/main/java/my/mins/jmt/app/send/controller/WebHook.java
@@ -1,7 +1,6 @@
 package my.mins.jmt.app.send.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
--- src/main/java/my/mins/jmt/common/controller/SystemCheck.java
+++ src/main/java/my/mins/jmt/common/controller/SystemCheck.java
@@ -1,8 +1,5 @@
 package my.mins.jmt.common.controller;
 
-import my.mins.jmt.common.entity.Test;
-import my.mins.jmt.common.service.TestRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,17 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/system")
 public class SystemCheck {
 
-    @Autowired
-    private TestRepository testRepository;
-
     @PostMapping("/check")
     public ResponseEntity<String> check(){
-
-        Test test = new Test();
-        test.setAa("aaa");
-
-        testRepository.save(test);
-
         return ResponseEntity.ok("OK");
     }
 }
