83db6171b98505de71fb8392ab45eda80399f3f8
minssogi <codedoctor119@gmail.com>
2020-01-17 15:55:53 +0900

    + Message send function implement

--- src/main/java/my/mins/jmt/app/recommend/service/impl/RecommendServiceImpl.java
+++ src/main/java/my/mins/jmt/app/recommend/service/impl/RecommendServiceImpl.java
@@ -1,8 +1,46 @@
 package my.mins.jmt.app.recommend.service.impl;
 
+import my.mins.jmt.app.common.cd.FoodTypeCd;
+import my.mins.jmt.app.food.entity.Food;
+import my.mins.jmt.app.food.service.impl.DiningCodeFoodInfo;
 import my.mins.jmt.app.recommend.service.RecommendService;
+import my.mins.jmt.app.send.dto.MessageDTO;
+import my.mins.jmt.app.send.entity.LineBotChat;
+import my.mins.jmt.app.send.service.impl.LineBotChatRepository;
+import my.mins.jmt.app.send.service.impl.SendLineMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.List;
 
 @Service
 public class RecommendServiceImpl implements RecommendService {
+
+	@Autowired
+	private DiningCodeFoodInfo diningCodeFoodInfo;
+
+	@Autowired
+	private SendLineMessage sendLineMessage;
+
+	@Autowired
+	private LineBotChatRepository lineBotChatRepository;
+
+	/**
+	 * 12시 25분이 되면 맛집 리스트 추천
+	 *
+	 * @throws IOException
+	 */
+	@Scheduled(cron = "0 20 12 * * *")
+	public void lunchRecommend() throws IOException {
+		List<LineBotChat> allGroupList = lineBotChatRepository.findAll();
+
+		List<Food> foodInfoList = diningCodeFoodInfo.getFoodInfoList("신논현역", FoodTypeCd.WESTERN);
+		StringBuilder foodInfoMessage = new StringBuilder();
+
+		foodInfoList.forEach(food -> foodInfoMessage.append(food.toString())); // get food info
+
+		allGroupList.forEach(group -> sendLineMessage.send(new MessageDTO(group.getSendTargetId(), foodInfoMessage.toString())));
+	}
 }
